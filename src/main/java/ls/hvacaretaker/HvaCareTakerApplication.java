403f3c1780c8bb4569753801802eb91dbd83f0b6
Luke <nycthbre@gmail.com>
2022-02-26 13:58:36 +0100

    Project creation

---
+++ src/main/java/ls/hvacaretaker/HvaCareTakerApplication.java
@@ -0,0 +1,13 @@
+package ls.hvacaretaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvaCareTakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvaCareTakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ls/hvacaretaker/HvaCareTakerApplicationTests.java
@@ -0,0 +1,13 @@
+package ls.hvacaretaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvaCareTakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
