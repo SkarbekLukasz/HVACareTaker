335ea90c44e02e7de84f67c21d2916b0d8bc6326
Luke <nycthbre@gmail.com>
2022-04-02 22:47:44 +0200

    Dodano widok i formularz kontaktowy.

--- src/main/java/ls/hvacaretaker/pagecontroller/MainController.java
+++ src/main/java/ls/hvacaretaker/pagecontroller/MainController.java
@@ -1,15 +1,23 @@
 package ls.hvacaretaker.pagecontroller;
 
+import ls.hvacaretaker.common.Message;
+import ls.hvacaretaker.commonlogic.EmailService;
 import ls.hvacaretaker.security.CustomUserDetails;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+    private final EmailService emailService;
+
+    public MainController(EmailService emailService) {
+        this.emailService = emailService;
+    }
 
     @GetMapping("/")
     public String getMain(Model model) {
@@ -33,5 +41,20 @@ public class MainController {
         CustomUserDetails userDetails = (CustomUserDetails) securityContext.getAuthentication().getPrincipal();
         model.addAttribute("userdetails", userDetails);
         return "accountdetails";
+    }
+
+    @GetMapping("/contact")
+    public String contactInfo(Model model) {
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String sendContactMessage(@RequestParam String emailinput,
+                                     @RequestParam String topicinput,
+                                     @RequestParam String messageinfo,
+                                     Model model) {
+        emailService.sendMail(emailinput, topicinput, messageinfo);
+        model.addAttribute("message", new Message("Sukces!", "Wiadomość wysłana pomyślnie."));
+        return "message";
     }
 }
