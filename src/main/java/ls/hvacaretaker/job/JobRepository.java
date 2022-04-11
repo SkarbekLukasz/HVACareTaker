f39fb3ca6431999f5fbeba2f3f551e7ae73f937d
Luke <nycthbre@gmail.com>
2022-04-11 20:44:53 +0200

    JavaDoc dla pakietu job

--- src/main/java/ls/hvacaretaker/job/JobRepository.java
+++ src/main/java/ls/hvacaretaker/job/JobRepository.java
@@ -3,6 +3,9 @@ package ls.hvacaretaker.job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repozytorium dla obiektów typu Job
+ */
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 }
--- src/main/java/ls/hvacaretaker/job/JobService.java
+++ src/main/java/ls/hvacaretaker/job/JobService.java
@@ -10,18 +10,38 @@ import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.Optional;
 
+/**
+ * Klasa warstwy usług typu Job
+ */
 @Service
 public class JobService {
     private final JobRepository jobRepository;
     private final JobMapper jobMapper;
     private final DeviceRepository deviceRepository;
 
+    /**
+     * Konstruktor klasy wstrzykujący poniższe zależności
+     *
+     * @param jobRepository    repozytorium typu Job
+     * @param jobMapper        klasa mapująca typu Job
+     * @param deviceRepository repozytorium typu Device
+     */
     public JobService(JobRepository jobRepository, JobMapper jobMapper, DeviceRepository deviceRepository) {
         this.jobRepository = jobRepository;
         this.jobMapper = jobMapper;
         this.deviceRepository = deviceRepository;
     }
 
+    /**
+     * Zapisuje nowy obiekt typu Job w bazie danych
+     *
+     * @param id         numer id
+     * @param user       Obiekt typu User, wykonujący czynność serwisową.
+     * @param jobenddate Data wykonania czynności serwisowej
+     * @param actionlog  Opis czynności serwisowej
+     * @param actiontype Rodzaj czynności serwisowej
+     * @throws DeviceNotFoundException nie odnaleziono urządzenia w bazie danych.
+     */
     @Transactional
     public void saveJob(Long id, CustomUserDetails user, LocalDate jobenddate, String actionlog, JobType actiontype) throws DeviceNotFoundException {
         JobDto jobTosave = new JobDto();
