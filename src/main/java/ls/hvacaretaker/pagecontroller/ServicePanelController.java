ee1e7b4b33948621cb69c56731e2b1d046c3597e
Luke <nycthbre@gmail.com>
2022-04-13 20:05:28 +0200

    JavaDoc dla pakietu controllers

--- src/main/java/ls/hvacaretaker/pagecontroller/ServicePanelController.java
+++ src/main/java/ls/hvacaretaker/pagecontroller/ServicePanelController.java
@@ -20,16 +20,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDate;
 
 
+/**
+ * Kontroler obsługujący adresy /servicepanel
+ *
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
 @Controller
 public class ServicePanelController {
     private final DeviceService deviceService;
     private final JobService jobService;
 
+    /**
+     * Konstruktor wstrzyykujący zależności
+     *
+     * @param deviceService warstwa usług typu Device
+     * @param jobService    warstwa usług typu Job
+     */
     public ServicePanelController(DeviceService deviceService, JobService jobService) {
         this.deviceService = deviceService;
         this.jobService = jobService;
     }
 
+    /**
+     * Metoda wyświetla formularz dodawania nowej czynności serwisowej.
+     *
+     * @param id              numer id
+     * @param model           model widoku
+     * @param securityContext kontekst bezpieczeństwa
+     * @return szablon widoku /servicepanel
+     */
     @GetMapping("/servicepanel/device/{id}/addjob")
     public String loadServicePanel(@PathVariable Long id, Model model, @CurrentSecurityContext SecurityContext securityContext) {
         DeviceDto deviceFound;
@@ -45,6 +66,17 @@ public class ServicePanelController {
         return "servicepanel";
     }
 
+    /**
+     * Metoda dodaje do bazy danych czynność serwisową do urządzenia o wskazanym id.
+     *
+     * @param id              numer id
+     * @param model           model widoku
+     * @param securityContext kontekst bezpieczeństwa
+     * @param jobenddate      data dodania czynności serwisowej
+     * @param actiontype      typ czynności serwisowej
+     * @param actionlog       opis czynności serwisowej
+     * @return szablon widoku /message
+     */
     @PostMapping("/servicepanel/device/{id}/addjob/success")
     public String addServiceJob(@PathVariable Long id,
                                 Model model,
--- src/main/java/ls/hvacaretaker/pagecontroller/UserController.java
+++ src/main/java/ls/hvacaretaker/pagecontroller/UserController.java
@@ -11,20 +11,46 @@ import org.springframework.web.bind.annotation.*;
 import javax.management.relation.RoleNotFoundException;
 import javax.validation.Valid;
 
+/**
+ * Kontroler obsługujący adresy rejestracji i zmiany hasła użytkownika.
+ *
+ * @author Luke
+ * @version 1.0
+ * @since 1.0
+ */
 @Controller
 public class UserController {
     private final UserService userService;
 
+    /**
+     * Konstruktor wstrzyykujący zależności
+     *
+     * @param userService warstwa usług typu User
+     */
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Przesyłą widok z formularzem rejestracji nowego użytkownika.
+     *
+     * @param model model widoku
+     * @return szablon widoku /register
+     */
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
         return "register";
     }
 
+    /**
+     * Metoda przyjmuje parametry z danymi użytkownika z formularza i zapisuje go do bazy danych.
+     *
+     * @param user          obiekt typu User przesłany z formularza
+     * @param bindingResult obiekt przechowujący błędy podczas rejestracji
+     * @param model         model widoku
+     * @return szablon widoku /message
+     */
     @PostMapping("/process_register")
     public String processRegister(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors()) {
@@ -42,12 +68,28 @@ public class UserController {
         }
     }
 
+    /**
+     * Wyświetla formularz zmiany hasła
+     *
+     * @param id    numer id użytkownika
+     * @param model model widoku
+     * @return szablon widoku /passwordchanger
+     */
     @GetMapping("/user/{id}/changepassword")
     public String changePasswordForm(@PathVariable Long id, Model model) {
         model.addAttribute("userid", id);
         return "passwordchanger";
     }
 
+    /**
+     * Zapisuje hasło zmienione przez uzytkownika.
+     *
+     * @param id          numer id
+     * @param model       model widoku
+     * @param oldPassword String z starym hasłem
+     * @param newPassword String z nowym hasłem
+     * @return szablon widoku /message
+     */
     @PostMapping("/user/{id}/changepassword")
     public String changePasswordPosted(@PathVariable Long id,
                                        Model model,
